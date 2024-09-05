6d793b2f8bfa950be8dfc6f429687d2dc0d75296
jubair <jubairislamasif@gmail.com>
2024-09-05 19:56:19 +0600

    ADDED: tutorial 5

---
+++ class-3/tutorial.py
@@ -0,0 +1,6 @@
+import pandas
+
+# Read and work with a file named 'words.csv'
+df = pandas.read_csv('words.csv')
+print(df) # This will display first few rows from the words.csv file
+ 
---
+++ class-5/tutorial.py
@@ -0,0 +1,11 @@
+print("hello world")
+print(5) 
+print("jubair islm asif \n st joseph's high school khulna")
+# comments
+"""This is an if-else statement.
+It will execute a block of code if a specified condition is true.
+If the condition is false then it will execute another block of code."""
+
+
+print("abvieuv \"nveeirf\" jfiwjfwfj")
+print("hey", 6, 7, sep="~",end=" 809\n")
